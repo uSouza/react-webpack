53a701264414c0e6fc4ec982d2ecb40d0184e748
Uesley S. Silva <uesleysilva@alunos.utfpr.edu.br>
2019-05-28 23:19:18 -0300

    Encerrada a configuração do repositório do corpo de aplicação react-webpack

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 'use strict'
 
-var React = require('react')
+import React from 'react'
 
-var Title = React.createClass({
+const App = React.createClass({
   render: function () {
-    return React.createElement('h1', null, 'Title')
+    return <h1>Application3</h1>
   }
 })
 
-module.exports = Title
+export default App
--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 'use strict'
 
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Title = require('./app');
+import App from './app'
+import React from 'react'
+import { render } from 'react-dom'
+import { AppContainer } from 'react-hot-loader'
 
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <NextApp />
+    </AppContainer>,
+    document.querySelector('[data-js="app"]')
+  )
+}
 
-ReactDOM.render(
-  React.createElement(Title),
-  document.querySelector('[data-js="app"]')
-)
+renderApp(App)
+
+if (module.hot) {
+  module.hot.accept('./app', () => {
+    const NextApp = require('./app').default
+    renderApp(NextApp)
+  })
+}
